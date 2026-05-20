2b581e7928f921bf56763ea9312a210bc20f122c
Riley Seow <rielsical@gmail.com>
2026-05-20 15:48:28 -0700

    UC-10: Add JSDocs to MAP.ts constants file.

--- src/constants/MAP.ts
+++ src/constants/MAP.ts
@@ -1,13 +1,22 @@
 import { LngLat, type LngLatBoundsLike } from 'maplibre-gl';
 
-export const MAP_CENTER = new LngLat(-118.25, 34.05); // Los Angeles
+/**
+ * @constant MAP_CENTER
+ * @description The initial center of the map - Los Angeles
+ */
+export const MAP_CENTER = new LngLat(-118.25, 34.05);
 
+/**
+ * @constant MAP_MAX_BOUNDS
+ * @description The maximum bounds of the map - Southern California region
+ */
 export const MAP_MAX_BOUNDS: Extract<
   LngLatBoundsLike,
   [number, number, number, number]
-> = [
-  // Southern California region
-  -121.81, 32.2, -113.9, 35.2,
-];
+> = [-121.81, 32.2, -113.9, 35.2];
 
+/**
+ * @constant MAP_ZOOM
+ * @description The initial zoom level of the map
+ */
 export const MAP_ZOOM = 10;
