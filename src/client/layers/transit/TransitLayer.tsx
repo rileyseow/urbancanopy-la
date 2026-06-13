1c87c9f096bf3a05b2b1af1326cce6533060020f
Riley Seow <rielsical@gmail.com>
2026-06-12 17:31:49 -0700

    UC-62: Edit transit routes layer 'beforeId' again - switch to 'label_other' to avoid console error (cannot be added before nonexistent transit stops layer when loaded first).

--- src/client/layers/transit/TransitLayer.tsx
+++ src/client/layers/transit/TransitLayer.tsx
@@ -15,7 +15,7 @@ const TransitLayer = ({
         id={MAP_LAYER_IDS.transitRoutes}
         source={MAP_SOURCE_IDS.transitRoutes}
         type='line'
-        beforeId={MAP_LAYER_IDS.transitStops}
+        beforeId='label_other' // see basemap style spec
         filter={['==', ['geometry-type'], 'LineString']}
         layout={{
           visibility: isLayerVisible ? 'visible' : 'none',
